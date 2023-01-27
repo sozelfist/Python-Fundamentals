ae7a0e746dcd519a6aa0a56b182749eb69999534
sozelsgd <nhantruongnguyen123@gmail.com>
2023-01-27 20:50:47 +0700

    feat(math/calculus): add implementation of calculus key-concepts

---
+++ math/calculus/limit.py
@@ -0,0 +1,23 @@
+import unittest
+from sympy import Symbol, Limit
+
+
+def limit(function, x, a):
+    """
+    function : function f(x)
+    x : independent variable
+    a : value that x approaches
+    """
+    x = Symbol(x)
+    limit = Limit(function, x, a)
+    return limit.doit()
+
+
+class TestLimit(unittest.TestCase):
+    def test_limit(self):
+        result = limit("x**2", "x", 0)
+        self.assertEqual(result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
