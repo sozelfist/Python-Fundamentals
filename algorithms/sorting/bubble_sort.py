aba44a25af6c144bdc1aa941e8766d1bd9b2c926
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-03 19:45:12 +0700

    refactor: eliminate swapped check, and renew test cases

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -6,23 +6,37 @@ def bubble_sort(arr: List[int]) -> List[int]:
     n = len(arr)
     for i in range(n):
         # check if the list is already sorted
-        swapped = False
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
-            break
+        for j in range(n - 1, i, -1):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
     return arr
 
 
 class TestBubbleSort(unittest.TestCase):
-    def test_bubble_sort(self):
-        self.assertEqual(bubble_sort([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(bubble_sort([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(bubble_sort([-1, 2, -3]), [-3, -1, 2])
-        self.assertEqual(bubble_sort([1]), [1])
-        self.assertEqual(bubble_sort([]), [])
+    def test_already_sorted(self):
+        arr = [1, 2, 3, 4, 5]
+        sorted_arr = bubble_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_reverse_sorted(self):
+        arr = [5, 4, 3, 2, 1]
+        sorted_arr = bubble_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_random_unsorted(self):
+        arr = [3, 5, 2, 1, 4]
+        sorted_arr = bubble_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_duplicates(self):
+        arr = [3, 5, 3, 1, 4]
+        sorted_arr = bubble_sort(arr)
+        self.assertEqual(sorted_arr, [1, 3, 3, 4, 5])
+
+    def test_negative_elements(self):
+        arr = [3, -5, 2, -1, 4]
+        sorted_arr = bubble_sort(arr)
+        self.assertEqual(sorted_arr, [-5, -1, 2, 3, 4])
 
 
 if __name__ == '__main__':
