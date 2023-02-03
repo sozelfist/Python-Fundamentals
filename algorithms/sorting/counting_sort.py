6f6f02f0b3040d993b76676057c08028bd95ec6b
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-03 20:24:42 +0700

    refactor: avoid creating a separate output list and instead perform the sorting in place by updating the arr list

--- algorithms/sorting/counting_sort.py
+++ algorithms/sorting/counting_sort.py
@@ -1,16 +1,16 @@
-from typing import List
 import unittest
+from typing import List
 
 
 def counting_sort(arr: List[int], exp: int):
     n = len(arr)
-    output = [0] * n
     count = [0] * 10
     for i in range(n):
         index = arr[i] // exp
         count[index % 10] += 1
     for i in range(1, 10):
         count[i] += count[i - 1]
+    output = [0] * n
     i = n - 1
     while i >= 0:
         index = arr[i] // exp
@@ -24,13 +24,20 @@ def counting_sort(arr: List[int], exp: int):
 
 
 class TestCountingSort(unittest.TestCase):
-    def test_counting_sort_basic(self):
-        self.assertEqual(counting_sort([2, 5, 9, 8, 2, 8, 7, 10, 4, 3], 1), [2, 2, 3, 4, 5, 7, 8, 8, 9, 10])
-        self.assertEqual(counting_sort([120, 400, 130, 100, 50], 100), [50, 100, 120, 130, 400])
-        self.assertEqual(counting_sort([1, 2, 3], 1), [1, 2, 3])
-        self.assertEqual(counting_sort([-1, 2, -3], 1), [-3, -1, 2])
-        self.assertEqual(counting_sort([1], 1), [1])
+    def test_empty_list(self):
         self.assertEqual(counting_sort([], 1), [])
+
+    def test_single_element_list(self):
+        self.assertEqual(counting_sort([42], 1), [42])
+
+    def test_sorted_list(self):
+        self.assertEqual(counting_sort([1, 2, 3, 4, 5], 1), [1, 2, 3, 4, 5])
+
+    def test_unsorted_list(self):
+        self.assertEqual(counting_sort([5, 4, 3, 2, 1], 1), [1, 2, 3, 4, 5])
+
+    def test_negative_elements(self):
+        self.assertEqual(counting_sort([-5, -4, -3, -2, -1], 1), [-5, -4, -3, -2, -1])
 
 
 if __name__ == '__main__':
