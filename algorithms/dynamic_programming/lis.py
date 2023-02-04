05df044035b2c8b15a58c3640cade2fd866e95fb
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 14:51:48 +0700

    refactor(algos/dynamic): use `max` to return lis result

--- algorithms/dynamic_programming/lis.py
+++ algorithms/dynamic_programming/lis.py
@@ -1,21 +1,19 @@
 import unittest
-from typing import List
+from typing import Any
 
 
-def lis(arr: List[int]) -> int:
+def lis(arr: Any) -> int:
+    if not arr:
+        return 0
     n = len(arr)
     lis = [1] * n
 
     for i in range(1, n):
         for j in range(0, i):
-            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
-                lis[i] = lis[j] + 1
+            if arr[i] > arr[j]:
+                lis[i] = max(lis[i], lis[j] + 1)
 
-    maximum = 0
-    for i in range(n):
-        maximum = max(maximum, lis[i])
-
-    return maximum
+    return max(lis)
 
 
 class TestLIS(unittest.TestCase):
