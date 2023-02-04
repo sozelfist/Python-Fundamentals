3089a41e3b811f7b55896b20abf860c43e96769e
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 15:11:49 +0700

    refactor(algos/backtrack): replace `subsets` func with `dfs`-a recursive approach

--- algorithms/backtracking/find_subsets.py
+++ algorithms/backtracking/find_subsets.py
@@ -1,19 +1,34 @@
+import unittest
 from typing import List
 
 
-def subsets(nums: List[int], subset: List[int], index: int) -> List[List[int]]:
-    if index == len(nums):
-        return [subset]
-    else:
-        with_num = subset + [nums[index]]
-        without_num = subset
-        return subsets(nums, with_num, index + 1) + subsets(nums, without_num, index + 1)
-
-
 def find_subsets(nums: List[int]) -> List[List[int]]:
-    return subsets(nums, [], 0)
+    subsets = []
+
+    def dfs(start, path):
+        subsets.append(path)
+        for i in range(start, len(nums)):
+            dfs(i + 1, path + [nums[i]])
+    dfs(0, [])
+    return subsets
+
+
+class TestFindSubsets(unittest.TestCase):
+    def test_find_subsets(self):
+        nums = [1, 2, 3]
+        expected_output = [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+        self.assertEqual(find_subsets(nums), expected_output)
+
+    def test_find_subsets_with_duplicates(self):
+        nums = [1, 2, 2]
+        expected_output = [[], [1], [1, 2], [1, 2, 2], [1, 2], [2], [2, 2], [2]]
+        self.assertEqual(find_subsets(nums), expected_output)
+
+    def test_find_subsets_with_empty_input(self):
+        nums = []
+        expected_output = [[]]
+        self.assertEqual(find_subsets(nums), expected_output)
 
 
 if __name__ == '__main__':
-    print(find_subsets([1, 2, 3]))
-    # Result: [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+    unittest.main()
