7c957c59793b478699bd922c61abefba1585e4be
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 14:16:13 +0700

    refactor(algos/greedy): replace `sort` with `sorted`

--- algorithms/greedy/fractional_knapsack.py
+++ algorithms/greedy/fractional_knapsack.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 
 
 def fractional_knapsack(items: List[Tuple[int, int]], capacity: int) -> float:
-    items.sort(key=lambda x: x[1] / x[0], reverse=True)
+    items = sorted(items, key=lambda x: x[1] / x[0], reverse=True)
     total_value = 0.0
     for weight, value in items:
         if capacity >= weight:
@@ -18,17 +18,25 @@ def fractional_knapsack(items: List[Tuple[int, int]], capacity: int) -> float:
 
 class TestFractionalKnapsack(unittest.TestCase):
     def test_fractional_knapsack(self):
-        items = [(5, 30), (8, 120), (7, 10), (0, 20), (4, 50), (5, 80), (2, 10)]
-        capacity = 20
-        self.assertEqual(fractional_knapsack(items, capacity), 240)
+        items = [(10, 60), (20, 100), (30, 120)]
+        capacity = 50
+        result = fractional_knapsack(items, capacity)
+        self.assertAlmostEqual(result, 240.0, delta=1e-9)
 
-        items = [(5, 20), (8, 100), (7, 10), (0, 20), (4, 50), (5, 80), (2, 10)]
+        items = [(10, 60), (20, 100), (30, 120)]
         capacity = 20
-        self.assertEqual(fractional_knapsack(items, capacity), 220)
+        result = fractional_knapsack(items, capacity)
+        self.assertAlmostEqual(result, 110.0, delta=1e-9)
 
-        items = [(5, 20), (8, 100), (7, 10), (0, 20), (4, 50), (5, 80), (2, 10)]
+        items = [(10, 60), (20, 100), (30, 120)]
+        capacity = 70
+        result = fractional_knapsack(items, capacity)
+        self.assertAlmostEqual(result, 280.0, delta=1e-9)
+
+        items = [(10, 60), (20, 100), (30, 120)]
         capacity = 0
-        self.assertEqual(fractional_knapsack(items, capacity), 0)
+        result = fractional_knapsack(items, capacity)
+        self.assertAlmostEqual(result, 0.0, delta=1e-9)
 
 
 if __name__ == '__main__':
