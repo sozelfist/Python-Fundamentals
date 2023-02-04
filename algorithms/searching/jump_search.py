c31d937163b55bb867c5b885f55d5037365242ff
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 13:16:35 +0700

    refactor: replace 2nd `while` with a `for` loop, merged two `while loops

--- algorithms/searching/jump_search.py
+++ algorithms/searching/jump_search.py
@@ -7,35 +7,37 @@ def jump_search(arr: List[int], x: int) -> Union[int, str]:
     n = len(arr)
     step = int(math.sqrt(n))
     prev = 0
-    while arr[min(step, n) - 1] < x:
+    while True:
+        if arr[min(step, n) - 1] >= x:
+            break
         prev = step
         step += int(math.sqrt(n))
         if prev >= n:
             return "Not Found"
-    while arr[prev] < x:
-        prev += 1
-        if prev == min(step, n):
-            return "Not Found"
-    if arr[prev] == x:
-        return prev
+    for i in range(prev, min(step, n)):
+        if arr[i] == x:
+            return i
     return "Not Found"
 
 
 class TestJumpSearch(unittest.TestCase):
-    def test_jump_search_basic(self):
-        self.assertEqual(jump_search([1, 2, 3, 4, 5, 6], 4), 3)
-        self.assertEqual(jump_search([1, 2, 3, 4, 5, 6], 6), 5)
-        self.assertEqual(jump_search([-1, 2, -3], -1), 0)
-        self.assertEqual(jump_search([1], 1), 0)
+    def test_jump_search_found(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        x = 5
+        result = jump_search(arr, x)
+        self.assertEqual(result, 4)
 
-    def test_jump_search_edge_cases(self):
-        self.assertEqual(jump_search([], 5), "Not Found")
-        self.assertEqual(jump_search([1, 2, 3, 4, 5, 6], 7), "Not Found")
-        self.assertEqual(jump_search([1, 2, 3, 4, 5, 6], 8), "Not Found")
-        self.assertEqual(jump_search([1, 3, 5, 7, 9, 11], 6), "Not Found")
-        self.assertEqual(jump_search([1, 1, 1, 1, 1, 1], 1), 0)
-        self.assertEqual(jump_search([-1, -1, -1, -1, -1], -1), 0)
-        self.assertEqual(jump_search([-1, 1, -1, 1, -1], 1), 1)
+    def test_jump_search_not_found(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        x = 11
+        result = jump_search(arr, x)
+        self.assertEqual(result, "Not Found")
+
+    def test_jump_search_duplicate(self):
+        arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10]
+        x = 5
+        result = jump_search(arr, x)
+        self.assertEqual(result, 4)
 
 
 if __name__ == '__main__':
