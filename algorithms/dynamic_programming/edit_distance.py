a10e951ee8e9473d53ed07bedd0fc0e4d63909ea
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 14:47:13 +0700

    refactor(algos/dynamic): use list comprehension to create array

--- algorithms/dynamic_programming/edit_distance.py
+++ algorithms/dynamic_programming/edit_distance.py
@@ -3,15 +3,16 @@ import unittest
 
 def edit_distance(s1: str, s2: str) -> int:
     m, n = len(s1), len(s2)
-    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
     for i in range(m + 1):
-        for j in range(n + 1):
-            if i == 0:
-                dp[i][j] = j
-            elif j == 0:
-                dp[i][j] = i
-            elif s1[i - 1] == s2[j - 1]:
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
             else:
                 dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
