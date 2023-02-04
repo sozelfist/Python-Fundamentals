8b7ec5f892a6f95bbbbd89e78794215bb5e23a33
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 09:56:29 +0700

    refactor: change function signature and prototype, update tests

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -3,34 +3,47 @@ import random
 from typing import List
 
 
-def quick_sort(arr: List[int], low: int, high: int) -> List[int]:
-    if low < high:
-        pi = partition(arr, low, high)
-        quick_sort(arr, low, pi)
-        quick_sort(arr, pi + 1, high)
+def quick_sort(arr: List[int]) -> List[int]:
+    def partition(arr: List[int], start: int, end: int) -> int:
+        pivot_index = random.randint(start, end)
+        pivot = arr[pivot_index]
+        arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+        i = start - 1
+        for j in range(start, end):
+            if arr[j] <= pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[end] = arr[end], arr[i + 1]
+        return i + 1
+
+    def sort(arr: List[int], start: int, end: int):
+        if start < end:
+            pivot_index = partition(arr, start, end)
+            sort(arr, start, pivot_index - 1)
+            sort(arr, pivot_index + 1, end)
+    sort(arr, 0, len(arr) - 1)
     return arr
 
 
-def partition(arr: List[int], low: int, high: int) -> int:
-    pivot_index = random.randint(low, high)
-    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
-    pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-
-
 class TestQuickSort(unittest.TestCase):
-    def test_quick_sort_basic(self):
-        self.assertEqual(quick_sort([3, 2, 1], 0, 2), [1, 2, 3])
-        self.assertEqual(quick_sort([1, 2, 3], 0, 2), [1, 2, 3])
-        self.assertEqual(quick_sort([-1, 2, -3], 0, 2), [-3, -1, 2])
-        self.assertEqual(quick_sort([1], 0, 0), [1])
-        self.assertEqual(quick_sort([], 0, 0), [])
+    def test_quick_sort(self):
+        arr = [3, 2, 1, 5, 4]
+        self.assertEqual(quick_sort(arr), [1, 2, 3, 4, 5])
+
+        arr = [1, 2, 3, 4, 5]
+        self.assertEqual(quick_sort(arr), [1, 2, 3, 4, 5])
+
+        arr = [5, 4, 3, 2, 1]
+        self.assertEqual(quick_sort(arr), [1, 2, 3, 4, 5])
+
+        arr = [-3, -2, -1, -5, -4]
+        self.assertEqual(quick_sort(arr), [-5, -4, -3, -2, -1])
+
+        arr = []
+        self.assertEqual(quick_sort(arr), [])
+
+        arr = [4, 2, 5, 2, 1, 3, 5, 4, 3]
+        self.assertEqual(quick_sort(arr), [1, 2, 2, 3, 3, 4, 4, 5, 5])
 
 
 if __name__ == '__main__':
