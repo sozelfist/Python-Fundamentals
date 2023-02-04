06cf72dc777e1ad7216417aacf989f6401c532f4
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 09:36:11 +0700

    refactor: update tests and refactor loops

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -3,23 +3,23 @@ from typing import List
 
 
 def insertion_sort(arr: List[int]) -> List[int]:
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j] :
-            arr[j + 1] = arr[j]
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
             j -= 1
-        arr[j + 1] = key
     return arr
 
 
 class TestInsertionSort(unittest.TestCase):
     def test_insertion_sort(self):
         self.assertEqual(insertion_sort([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(insertion_sort([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(insertion_sort([-1, 2, -3]), [-3, -1, 2])
+        self.assertEqual(insertion_sort([5, 2, 9, 1, 5]), [1, 2, 5, 5, 9])
+        self.assertEqual(insertion_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertEqual(insertion_sort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         self.assertEqual(insertion_sort([1]), [1])
+        self.assertEqual(insertion_sort([3, 2, 1, 2, 3]), [1, 2, 2, 3, 3])
+        self.assertEqual(insertion_sort([-5, 2, 9, 1, -2]), [-5, -2, 1, 2, 9])
         self.assertEqual(insertion_sort([]), [])
 
 
