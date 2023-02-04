d9cd5a3e212b91d78b65bbd0004666c80f30d392
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 10:22:00 +0700

    refactor(tests): update tests: duplicate, empty, negative elemements

--- algorithms/sorting/shell_sort.py
+++ algorithms/sorting/shell_sort.py
@@ -5,6 +5,7 @@ from typing import List
 def shell_sort(arr: List[int]) -> List[int]:
     n = len(arr)
     gap = n // 2
+
     while gap > 0:
         for i in range(gap, n):
             temp = arr[i]
@@ -18,18 +19,35 @@ def shell_sort(arr: List[int]) -> List[int]:
 
 
 class TestShellSort(unittest.TestCase):
-    def test_shell_sort_basic(self):
-        self.assertEqual(shell_sort([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(shell_sort([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(shell_sort([-1, 2, -3]), [-3, -1, 2])
-        self.assertEqual(shell_sort([1]), [1])
-        self.assertEqual(shell_sort([]), [])
+    def test_already_sorted(self):
+        arr = [1, 2, 3, 4, 5]
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
 
-    def test_shell_sort_performance(self):
-        import random
-        for i in range(0, 10):
-            arr = random.sample(range(-10000, 10000), 10000)
-            self.assertEqual(shell_sort(arr), sorted(arr))
+    def test_reverse_sorted(self):
+        arr = [5, 4, 3, 2, 1]
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_random_unsorted(self):
+        arr = [3, 5, 2, 1, 4]
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_duplicates(self):
+        arr = [3, 5, 3, 1, 4]
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [1, 3, 3, 4, 5])
+
+    def test_negative_elements(self):
+        arr = [3, -5, 2, -1, 4]
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [-5, -1, 2, 3, 4])
+
+    def test_empty_list(self):
+        arr = []
+        sorted_arr = shell_sort(arr)
+        self.assertEqual(sorted_arr, [])
 
 
 if __name__ == '__main__':
