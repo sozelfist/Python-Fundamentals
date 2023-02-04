ba57dcec54dad12bf83899562e2bdc7693aa223f
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 10:17:47 +0700

    refactor: add element checking before swap, update tests

--- algorithms/sorting/selection_sort.py
+++ algorithms/sorting/selection_sort.py
@@ -1,5 +1,5 @@
-import unittest
 from typing import List
+import unittest
 
 
 def selection_sort(arr: List[int]) -> List[int]:
@@ -9,17 +9,41 @@ def selection_sort(arr: List[int]) -> List[int]:
         for j in range(i + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        if i != min_idx:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
 
 class TestSelectionSort(unittest.TestCase):
-    def test_selection_sort(self):
-        self.assertEqual(selection_sort([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(selection_sort([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(selection_sort([-1, 2, -3]), [-3, -1, 2])
-        self.assertEqual(selection_sort([1]), [1])
-        self.assertEqual(selection_sort([]), [])
+    def test_already_sorted(self):
+        arr = [1, 2, 3, 4, 5]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_reverse_sorted(self):
+        arr = [5, 4, 3, 2, 1]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_random_unsorted(self):
+        arr = [3, 5, 2, 1, 4]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [1, 2, 3, 4, 5])
+
+    def test_duplicates(self):
+        arr = [3, 5, 3, 1, 4]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [1, 3, 3, 4, 5])
+
+    def test_negative_elements(self):
+        arr = [3, -5, 2, -1, 4]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [-5, -1, 2, 3, 4])
+
+    def test_empty_list(self):
+        arr = []
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted_arr, [])
 
 
 if __name__ == '__main__':
