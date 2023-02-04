d58ee5fa2401fcf31b836d300d2e0452438684d8
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 13:22:07 +0700

    refactor: handle search on empty list, update tests

--- algorithms/searching/linear_search.py
+++ algorithms/searching/linear_search.py
@@ -3,24 +3,36 @@ from typing import List, Union
 
 
 def linear_search(arr: List[int], x: int) -> Union[int, str]:
-    for i in range(len(arr)):
-        if arr[i] == x:
+    for i, value in enumerate(arr):
+        if value == x:
             return i
     return "Not Found"
 
 
 class TestLinearSearch(unittest.TestCase):
-    def test_linear_search_basic(self):
-        self.assertEqual(linear_search([1, 2, 3, 4, 5, 6], 4), 3)
-        self.assertEqual(linear_search([1, 2, 3, 4, 5, 6], 6), 5)
-        self.assertEqual(linear_search([-1, 2, -3], -1), 0)
-        self.assertEqual(linear_search([1], 1), 0)
-        self.assertEqual(linear_search([], 5), "Not Found")
+    def test_linear_search_found(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        x = 5
+        result = linear_search(arr, x)
+        self.assertEqual(result, 4)
 
-    def test_linear_search_edge_cases(self):
-        self.assertEqual(linear_search([1, 2, 3, 4, 5, 6], 7), "Not Found")
-        self.assertEqual(linear_search([], None), "Not Found")
-        self.assertEqual(linear_search([1, 2, 3, 4, 5, 6], '4'), "Not Found")
+    def test_linear_search_not_found(self):
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        x = 11
+        result = linear_search(arr, x)
+        self.assertEqual(result, "Not Found")
+
+    def test_linear_search_empty_list(self):
+        arr = []
+        x = 11
+        result = linear_search(arr, x)
+        self.assertEqual(result, "Not Found")
+
+    def test_duplicate_elements(self):
+        arr = [1, 2, 3, 4, 4, 5, 6]
+        x = 4
+        result = linear_search(arr, x)
+        self.assertIn(result, [3, 4], "Incorrect result with duplicate elements")
 
 
 if __name__ == '__main__':
