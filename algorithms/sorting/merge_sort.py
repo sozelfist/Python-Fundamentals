89b02eef88b3b1a796648c082b349632de437a13
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-04 09:42:54 +0700

    refactor : use descriptive variable names (PEP8) and update tests

--- algorithms/sorting/merge_sort.py
+++ algorithms/sorting/merge_sort.py
@@ -2,19 +2,19 @@ import unittest
 from typing import List
 
 
-def merge(left: List[int], right: List[int]) -> List[int]:
-    result = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
+def merge_lists(left: List[int], right: List[int]) -> List[int]:
+    merged = []
+    left_index = right_index = 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
         else:
-            result.append(right[j])
-            j += 1
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
+            merged.append(right[right_index])
+            right_index += 1
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+    return merged
 
 
 def merge_sort(arr: List[int]) -> List[int]:
@@ -23,15 +23,32 @@ def merge_sort(arr: List[int]) -> List[int]:
     mid = len(arr) // 2
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
-    return merge(left, right)
+    return merge_lists(left, right)
 
 
 class TestMergeSort(unittest.TestCase):
-    def test_merge_sort(self):
-        self.assertEqual(merge_sort([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(merge_sort([1, 2, 3]), [1, 2, 3])
-        self.assertEqual(merge_sort([-1, 2, -3]), [-3, -1, 2])
-        self.assertEqual(merge_sort([1]), [1])
+    def test_empty_list(self):
+        self.assertEqual(merge_sort([]), [])
+
+    def test_list_of_length_one(self):
+        self.assertEqual(merge_sort([5]), [5])
+
+    def test_sorting_of_already_sorted_list(self):
+        self.assertEqual(merge_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_sorting_of_unsorted_list(self):
+        self.assertEqual(merge_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+
+    def test_sorting_of_list_with_duplicate_elements(self):
+        self.assertEqual(merge_sort([5, 4, 5, 2, 1]), [1, 2, 4, 5, 5])
+
+    def test_sorting_of_list_with_all_duplicate_elements(self):
+        self.assertEqual(merge_sort([0, 0, 0, 0, 0]), [0, 0, 0, 0, 0])
+
+    def test_sorting_of_list_with_negative_elements(self):
+        self.assertEqual(merge_sort([-1, -5, -3, -2, -4]), [-5, -4, -3, -2, -1])
+
+    def test_sorting_of_list_with_empty_list(self):
         self.assertEqual(merge_sort([]), [])
 
 
