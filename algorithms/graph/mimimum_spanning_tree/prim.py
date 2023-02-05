288d0382ed517950a9dbae673f45ced4b6167df4
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-05 10:54:55 +0700

    refactor(prim): make utils functions and reimplement prim algos, update tests

--- algorithms/graph/mimimum_spanning_tree/prim.py
+++ algorithms/graph/mimimum_spanning_tree/prim.py
@@ -1,58 +1,77 @@
 import unittest
-from typing import List, Tuple
 import heapq
+from typing import List, Tuple
 
 
 def prim(n: int, edges: List[Tuple[int, int, int]], start: int) -> List[Tuple[int, int]]:
-    graph = [[] for _ in range(n)]
-    for u, v, w in edges:
-        graph[u].append((v, w))
-        graph[v].append((u, w))
+    if n == 0:
+        return []
+
+    graph = create_graph(n, edges)
     mst = []
     visited = [False] * n
     heap = [(0, start, -1)]
     heapq.heapify(heap)
     while heap:
-        w, u, p = heapq.heappop(heap)
-        if visited[u]:
+        weight, node, parent = heapq.heappop(heap)
+        if visited[node]:
             continue
-        visited[u] = True
-        if p != -1:
-            mst.append((p, u))
-        for v, weight in graph[u]:
-            if not visited[v]:
-                heapq.heappush(heap, (weight, v, u))
+        visited[node] = True
+        if parent != -1:
+            mst.append((parent, node))
+        add_neighbors_to_heap(graph, node, visited, heap)
     return mst
 
 
+def create_graph(n: int, edges: List[Tuple[int, int, int]]) -> List[List[Tuple[int, int]]]:
+    graph = [[] for _ in range(n)]
+    for u, v, w in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+    return graph
+
+
+def add_neighbors_to_heap(
+    graph: List[List[Tuple[int, int]]],
+    node: int, visited: List[bool],
+    heap: List[Tuple[int, int, int]]
+):
+    for neighbor, weight in graph[node]:
+        if not visited[neighbor]:
+            heapq.heappush(heap, (weight, neighbor, node))
+
+
 class TestPrim(unittest.TestCase):
-    def test_valid_input(self):
-        # Test case 1: a valid input
-        n = 4
-        edges = [(0, 1, 1), (0, 2, 3), (1, 2, 1), (2, 3, 5)]
+    def test_empty_graph(self):
+        n = 0
+        edges = []
         start = 0
         result = prim(n, edges, start)
-        expected_result = [(0, 1), (1, 2), (0, 2)]
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, [])
 
-    def test_disconnected_graph(self):
-        # Test case 2: a disconnected graph
+    def test_single_node(self):
+        n = 1
+        edges = []
+        start = 0
+        result = prim(n, edges, start)
+        self.assertEqual(result, [])
+
+    def test_small_graph(self):
         n = 5
-        edges = [(0, 1, 3), (2, 3, 2), (3, 4, 4)]
+        edges = [(0, 1, 1), (0, 2, 2), (1, 2, 3), (1, 3, 4), (2, 3, 5), (3, 4, 6)]
         start = 0
         result = prim(n, edges, start)
-        expected_result = []
-        self.assertEqual(result, expected_result)
+        expected = [(0, 1), (0, 2), (1, 3), (3, 4)]
+        self.assertCountEqual(result, expected)
 
-    def test_large_input(self):
-        # Test case 3: a large input
+    def test_large_graph(self):
         n = 10
-        edges = [(0, 1, 5), (0, 2, 3), (1, 2, 1), (2, 3, 5), (3, 4, 8),
-                 (4, 5, 2), (5, 6, 9), (6, 7, 6), (7, 8, 3), (8, 9, 1)]
+        edges = [(0, 1, 1), (0, 2, 2), (1, 2, 3), (1, 3, 4), (2, 3, 5), (3, 4, 6),
+                 (4, 5, 7), (5, 6, 8), (6, 7, 9), (7, 8, 10), (8, 9, 11)]
         start = 0
         result = prim(n, edges, start)
-        expected_result = [(0, 2), (2, 1), (4, 5), (8, 9), (7, 8), (6, 7)]
-        self.assertEqual(result, expected_result)
+        expected = [(0, 1), (1, 3), (0, 2), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
+        self.assertCountEqual(result, expected)
 
 
 if __name__ == '__main__':
