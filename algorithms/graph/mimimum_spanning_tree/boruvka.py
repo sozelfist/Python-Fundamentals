d70ee4dbeab6dfb776c0b61b59b04ece30639c3a
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-05 11:10:02 +0700

    refactor(boruvka): handle disconected and empty graph

--- algorithms/graph/mimimum_spanning_tree/boruvka.py
+++ algorithms/graph/mimimum_spanning_tree/boruvka.py
@@ -1,54 +1,57 @@
-import unittest
 from typing import List, Tuple
+import unittest
 
 
 def boruvka(n: int, edges: List[Tuple[int, int, int]]) -> List[Tuple[int, int]]:
     mst = []
     rep = [i for i in range(n)]
     weight = [float('inf')] * n
+
     while len(mst) < n - 1:
+        connected_components = n
         for i in range(n):
             rep[i] = i
+
         for u, v, w in edges:
-            if rep[u] != rep[v]:
-                ru, rv = rep[u], rep[v]
-                if w < weight[ru]:
-                    weight[ru] = w
-                    mst.append((u, v))
-                if w < weight[rv]:
-                    weight[rv] = w
-                    mst.append((u, v))
-                for i in range(n):
-                    if rep[i] == ru:
-                        rep[i] = rv
+            ru, rv = rep[u], rep[v]
+
+            if ru != rv:
+                if w < min(weight[ru], weight[rv]):
+                    if weight[ru] < weight[rv]:
+                        weight[rv] = w
+                        mst.append((u, v))
+                        rep = [rv if r == ru else r for r in rep]
+                        connected_components -= 1
+                    else:
+                        weight[ru] = w
+                        mst.append((u, v))
+                        rep = [ru if r == rv else r for r in rep]
+                        connected_components -= 1
+
+        if connected_components == n:
+            break
+
     return mst
 
 
 class TestBoruvka(unittest.TestCase):
-    def test_valid_input(self):
-        # Test case 1: a valid input
+    def test_boruvka_empty_graph(self):
+        n = 0
+        edges = []
+        result = boruvka(n, edges)
+        self.assertEqual(result, [])
+
+    def test_boruvka_complete_graph(self):
         n = 4
-        edges = [(0, 1, 1), (0, 2, 3), (1, 2, 1), (2, 3, 5)]
+        edges = [(0, 1, 1), (0, 2, 2), (0, 3, 3), (1, 2, 4), (1, 3, 5), (2, 3, 6)]
         result = boruvka(n, edges)
-        expected_result = [(0, 1), (1, 2), (0, 2)]
-        self.assertEqual(result, expected_result)
+        self.assertEqual(len(result), n - 1)
 
-    def test_disconnected_graph(self):
-        # Test case 2: a disconnected graph
-        n = 5
-        edges = [(0, 1, 3), (2, 3, 2), (3, 4, 4)]
+    def test_boruvka_disconnected_graph(self):
+        n = 4
+        edges = [(0, 1, 1), (2, 3, 2)]
         result = boruvka(n, edges)
-        expected_result = []
-        self.assertEqual(result, expected_result)
-
-    def test_large_input(self):
-        # Test case 3: a large input
-        n = 10
-        edges = [(0, 1, 5), (0, 2, 3), (1, 2, 1), (2, 3, 5), (3, 4, 8),
-                 (4, 5, 2), (5, 6, 9), (6, 7, 6), (7, 8, 3), (8, 9, 1)]
-        result = boruvka(n, edges)
-        expected_result = [(0, 2), (2, 1), (4, 5), (8, 9), (7, 8), (6, 7)]
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, [(0, 1), (2, 3)])
 
 
 if __name__ == '__main__':
