f1375f0cc1b99d27fd8fc8f3b739abbfac6e7858
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-05 14:12:35 +0700

    refactor(algos/grap/topo_sort): minor change on code, update tests

--- algorithms/graph/topological_sort/dfs_topo_sort.py
+++ algorithms/graph/topological_sort/dfs_topo_sort.py
@@ -3,53 +3,72 @@ from typing import List, Dict
 
 
 def topological_sort(graph: Dict[int, List[int]]) -> List[int]:
-    def dfs(v: int, visited: List[bool], stack: List[int]):
-        visited[v] = True
-        for neighbor in graph[v]:
+    def dfs(node: int, visited: List[bool], stack: List[int]):
+        visited[node] = True
+        for neighbor in graph[node]:
             if not visited[neighbor]:
                 dfs(neighbor, visited, stack)
-        stack.append(v)
+        stack.append(node)
 
     n = len(graph)
-    visited = [False for _ in range(n)]
+    visited = [False] * n
     stack = []
-    for i in range(n):
-        if not visited[i]:
-            dfs(i, visited, stack)
-    return stack[::-1]
+    for node in range(n):
+        if not visited[node]:
+            dfs(node, visited, stack)
+    return list(reversed(stack))
 
 
-class TestTopologicalSort(unittest.TestCase):
-    def test_simple(self):
-        graph = {
-            0: [],
-            1: [],
-            2: [3],
-            3: [1],
-            4: [0, 1],
-            5: [2, 0]
-        }
-        self.assertEqual(topological_sort(graph), [5, 4, 2, 3, 1, 0])
-
-    def test_complex(self):
+class TopologicalSortTest(unittest.TestCase):
+    def test_case_1(self):
         graph = {
             0: [1, 2],
             1: [3],
             2: [3],
-            3: [4],
-            4: [5],
-            5: []
+            3: []
         }
-        self.assertEqual(topological_sort(graph), [0, 2, 1, 3, 4, 5])
+        result = topological_sort(graph)
+        self.assertListEqual(result, [0, 2, 1, 3])
 
-    def test_cycle(self):
+    def test_case_2(self):
         graph = {
-            0: [1],
-            1: [2],
-            2: [0]
+            0: [2, 0],
+            1: [0, 1],
+            2: [3],
+            3: [1],
         }
-        with self.assertRaises(Exception):
-            topological_sort(graph)
+        result = topological_sort(graph)
+        self.assertListEqual(result, [0, 2, 3, 1])
+
+    def test_case_3(self):
+        graph = {
+            0: [],
+            1: [],
+            2: []
+        }
+        result = topological_sort(graph)
+        self.assertListEqual(result, [2, 1, 0])
+
+    def test_case_4(self):
+        graph = {
+            0: [1, 2],
+            1: [2],
+            2: [3],
+            3: [4],
+            4: []
+        }
+        result = topological_sort(graph)
+        self.assertListEqual(result, [0, 1, 2, 3, 4])
+
+    def test_case_5(self):
+        graph = {
+            3: [2, 0],
+            2: [1],
+            1: [0],
+            0: []
+        }
+        result = topological_sort(graph)
+        self.assertListEqual(result, [3, 2, 1, 0])
 
 
 if __name__ == '__main__':
--- algorithms/graph/topological_sort/kahn.py
+++ algorithms/graph/topological_sort/kahn.py
@@ -3,73 +3,42 @@ from typing import List, Dict
 
 
 def kahn_topological_sort(graph: Dict[int, List[int]]) -> List[int]:
-    indegrees = [0 for _ in range(len(graph))]
-    for v in graph:
-        for neighbor in graph[v]:
+    indegrees = [0] * len(graph)
+    for node in graph:
+        for neighbor in graph[node]:
             indegrees[neighbor] += 1
-    queue = []
-    for i in range(len(indegrees)):
-        if indegrees[i] == 0:
-            queue.append(i)
+    queue = [node for node, degree in enumerate(indegrees) if degree == 0]
     result = []
     while queue:
-        u = queue.pop(0)
-        result.append(u)
-        for neighbor in graph[u]:
+        node = queue.pop(0)
+        result.append(node)
+        for neighbor in graph[node]:
             indegrees[neighbor] -= 1
             if indegrees[neighbor] == 0:
                 queue.append(neighbor)
-    if len(result) != len(graph):
-        return []
-    return result
+    return result if len(result) == len(graph) else []
 
 
 class TestKahnTopologicalSort(unittest.TestCase):
-    def test_simple(self):
-        graph = {
-            0: [],
-            1: [],
-            2: [3],
-            3: [1],
-            4: [0, 1],
-            5: [2, 0]
-        }
-        self.assertEqual(kahn_topological_sort(graph), [4, 5, 2, 0, 3, 1])
-
-    def test_complex(self):
-        graph = {
-            0: [1, 2],
-            1: [3],
-            2: [3],
-            3: [4],
-            4: [5],
-            5: []
-        }
-        self.assertEqual(kahn_topological_sort(graph), [0, 1, 2, 3, 4, 5])
-
-    def test_cycle(self):
-        graph = {
-            0: [1],
-            1: [2],
-            2: [0]
-        }
-        self.assertEqual(kahn_topological_sort(graph), [])
-
-    def test_disconnected_graph(self):
-        graph = {
-            0: [1, 2],
-            1: [],
-            2: [3],
-            3: [4],
-            4: [],
-            5: [],
-            6: [],
-        }
-        self.assertEqual(kahn_topological_sort(graph), [0, 5, 6, 1, 2, 3, 4])
-
     def test_empty_graph(self):
         graph = {}
         self.assertEqual(kahn_topological_sort(graph), [])
+
+    def test_single_node_graph(self):
+        graph = {0: []}
+        self.assertEqual(kahn_topological_sort(graph), [0])
+
+    def test_dag(self):
+        graph = {0: [1, 2], 1: [2], 2: []}
+        self.assertEqual(kahn_topological_sort(graph), [0, 1, 2])
+
+    def test_cyclic_graph(self):
+        graph = {0: [1], 1: [2], 2: [0]}
+        self.assertEqual(kahn_topological_sort(graph), [])
+
+    def test_complex_dag(self):
+        graph = {0: [1, 2, 3], 1: [2, 3], 2: [4], 3: [4], 4: []}
+        self.assertEqual(kahn_topological_sort(graph), [0, 1, 2, 3, 4])
 
 
 if __name__ == '__main__':
