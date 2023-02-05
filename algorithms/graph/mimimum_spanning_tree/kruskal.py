c70c01d6313134e5c3e0d4b5ff110e257f440f44
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-05 11:01:45 +0700

    refactor(kruskal): use of more descriptive and semantic function names

--- algorithms/graph/mimimum_spanning_tree/kruskal.py
+++ algorithms/graph/mimimum_spanning_tree/kruskal.py
@@ -5,46 +5,41 @@ from typing import List, Tuple
 def kruskal(n: int, edges: List[Tuple[int, int, int]]) -> List[Tuple[int, int]]:
     edges.sort(key=lambda x: x[2])
     parent = [i for i in range(n)]
+    result = []
 
-    def find(x: int) -> int:
+    def find_parent(x: int) -> int:
         if parent[x] != x:
-            parent[x] = find(parent[x])
+            parent[x] = find_parent(parent[x])
         return parent[x]
 
-    def union(x: int, y: int) -> None:
-        parent[find(x)] = find(y)
-    result = []
+    def merge_sets(x: int, y: int) -> None:
+        parent[find_parent(x)] = find_parent(y)
+
     for u, v, w in edges:
-        if find(u) != find(v):
+        if find_parent(u) != find_parent(v):
             result.append((u, v))
-            union(u, v)
+            merge_sets(u, v)
     return result
 
 
 class TestKruskal(unittest.TestCase):
-    def test_valid_input(self):
-        # Test case 1: a valid input
+    def test_kruskal(self):
         n = 4
-        edges = [(0, 1, 1), (0, 2, 3), (1, 2, 1), (2, 3, 5)]
+        edges = [(0, 1, 10), (0, 2, 6), (0, 3, 5), (1, 3, 15), (2, 3, 4)]
         result = kruskal(n, edges)
-        expected_result = [(0, 1), (1, 2), (0, 2)]
+        expected_result = [(2, 3), (0, 3), (0, 1)]
         self.assertEqual(result, expected_result)
 
-    def test_disconnected_graph(self):
-        # Test case 2: a disconnected graph
         n = 5
-        edges = [(0, 1, 3), (2, 3, 2), (3, 4, 4)]
+        edges = [(0, 1, 2), (0, 3, 6), (1, 2, 3), (3, 4, 5), (4, 2, 1)]
         result = kruskal(n, edges)
-        expected_result = []
+        expected_result = [(4, 2), (0, 1), (1, 2), (3, 4)]
         self.assertEqual(result, expected_result)
 
-    def test_large_input(self):
-        # Test case 3: a large input
-        n = 10
-        edges = [(0, 1, 5), (0, 2, 3), (1, 2, 1), (2, 3, 5), (3, 4, 8),
-                 (4, 5, 2), (5, 6, 9), (6, 7, 6), (7, 8, 3), (8, 9, 1)]
+        n = 6
+        edges = [(0, 1, 2), (0, 3, 6), (1, 2, 3), (3, 4, 5), (4, 2, 1), (5, 2, 4)]
         result = kruskal(n, edges)
-        expected_result = [(0, 2), (2, 1), (4, 5), (8, 9), (7, 8), (6, 7)]
+        expected_result = [(4, 2), (0, 1), (1, 2), (5, 2), (3, 4)]
         self.assertEqual(result, expected_result)
 
 
