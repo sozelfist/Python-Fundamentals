bcbc4de7d84eca1749e338e6643fe6301c7855bf
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-07 10:25:42 +0700

    feat(conversions): add conversions implementation of decimal to any base

---
+++ conversions/decimal_to_base.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+def decimal_to_base(decimal: int, base: int) -> str:
+    if decimal == 0:
+        return "0"
+    elif decimal < 0 or base < 2:
+        raise ValueError("Invalid input: decimal must be a positive integer and base must be 2 or greater")
+
+    digits = "0123456789ABCDEF"
+    result = ""
+    while decimal > 0:
+        result = digits[decimal % base] + result
+        decimal = decimal // base
+    return result
+
+
+class TestDecimalToBase(unittest.TestCase):
+    def test_binary(self):
+        self.assertEqual(decimal_to_base(30, 2), "11110")
+
+    def test_octal(self):
+        self.assertEqual(decimal_to_base(30, 8), "36")
+
+    def test_hexadecimal(self):
+        self.assertEqual(decimal_to_base(30, 16), "1E")
+
+    def test_zero_decimal(self):
+        self.assertEqual(decimal_to_base(0, 2), "0")
+
+    def test_negative_decimal(self):
+        with self.assertRaises(ValueError):
+            decimal_to_base(-30, 2)
+
+    def test_base_less_than_2(self):
+        with self.assertRaises(ValueError):
+            decimal_to_base(30, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ conversions/decimal_to_binary.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+def decimal_to_binary(decimal: int) -> str:
+    if decimal == 0:
+        return "0"
+    binary = []
+    while decimal > 0:
+        binary.append(str(decimal % 2))
+        decimal = decimal // 2
+    return "".join(reversed(binary))
+
+
+class TestDecimalToBinary(unittest.TestCase):
+    def test_decimal_to_binary(self):
+        self.assertEqual(decimal_to_binary(0), "0")
+        self.assertEqual(decimal_to_binary(1), "1")
+        self.assertEqual(decimal_to_binary(2), "10")
+        self.assertEqual(decimal_to_binary(3), "11")
+        self.assertEqual(decimal_to_binary(15), "1111")
+        self.assertEqual(decimal_to_binary(16), "10000")
+
+
+if __name__ == '__main__':
+    unittest.main()
