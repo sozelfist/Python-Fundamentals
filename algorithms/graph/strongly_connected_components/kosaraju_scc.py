75b5f01bd588f35d3405203401b6a5e6b042bd14
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-13 14:44:25 +0700

    feat(algos/graph/scc): implement strongly connected components - Kosaraju Algorithm - Tarjan Algorithm

---
+++ algorithms/graph/strongly_connected_components/kosaraju_scc.py
@@ -0,0 +1,60 @@
+import unittest
+from typing import List
+
+
+def kosaraju(graph: List[List[int]]) -> List[List[int]]:
+    def dfs_first_pass(node: int, visited: List[bool], stack: List[int]) -> None:
+        visited[node] = True
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                dfs_first_pass(neighbor, visited, stack)
+        stack.append(node)
+
+    def dfs_second_pass(node: int, visited: List[bool], component: List[int]) -> None:
+        visited[node] = True
+        component.append(node)
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                dfs_second_pass(neighbor, visited, component)
+
+    n = len(graph)
+    visited = [False for _ in range(n)]
+    stack = []
+
+    for i in range(n):
+        if not visited[i]:
+            dfs_first_pass(i, visited, stack)
+
+    reversed_graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in graph[i]:
+            reversed_graph[j].append(i)
+
+    visited = [False for _ in range(n)]
+    result = []
+
+    while stack:
+        node = stack.pop()
+        if not visited[node]:
+            component = []
+            dfs_second_pass(node, visited, component)
+            result.append(component)
+
+    return result
+
+
+class TestKosaraju(unittest.TestCase):
+    def test_kosaraju(self):
+        graph = [
+            [1],
+            [2],
+            [0, 3],
+            [2]
+        ]
+        expected = [[0, 1, 2, 3]]
+        result = kosaraju(graph)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ algorithms/graph/strongly_connected_components/tarjan_scc.py
@@ -0,0 +1,75 @@
+from collections import deque
+import unittest
+
+
+def create_graph(num_vertices, edges):
+    graph = [[] for _ in range(num_vertices)]
+    for u, v in edges:
+        graph[u].append(v)
+    return graph
+
+
+def strongly_connected_components(graph):
+    num_vertices = len(graph)
+    stack = deque()
+    on_stack = [False] * num_vertices
+    indices = [-1] * num_vertices
+    low_links = indices[:]
+    index = 0
+    components = []
+
+    def tarjan_dfs(v):
+        nonlocal index
+        indices[v] = index
+        low_links[v] = index
+        index += 1
+        stack.append(v)
+        on_stack[v] = True
+
+        for w in graph[v]:
+            if indices[w] == -1:
+                tarjan_dfs(w)
+                low_links[v] = min(low_links[v], low_links[w])
+            elif on_stack[w]:
+                low_links[v] = min(low_links[v], indices[w])
+
+        if low_links[v] == indices[v]:
+            component = []
+            w = stack.pop()
+            on_stack[w] = False
+            component.append(w)
+            while w != v:
+                w = stack.pop()
+                on_stack[w] = False
+                component.append(w)
+            components.append(component)
+
+    for v in range(num_vertices):
+        if indices[v] == -1:
+            tarjan_dfs(v)
+
+    return components
+
+
+class TestTarjan(unittest.TestCase):
+    def test_empty_graph(self):
+        self.assertEqual(strongly_connected_components([]), [])
+
+    def test_graph_with_single_node(self):
+        self.assertEqual(strongly_connected_components([[]]), [[0]])
+
+    def test_strongly_connected_components_of_size_1(self):
+        g = create_graph(5, [(0, 1), (1, 2), (2, 0)])
+        self.assertEqual(strongly_connected_components(g), [[2, 1, 0], [3], [4]])
+
+    def test_strongly_connected_components_of_size_2(self):
+        g = create_graph(5, [(0, 1), (1, 2), (2, 0), (3, 4), (4, 3)])
+        self.assertEqual(strongly_connected_components(g), [[2, 1, 0], [4, 3]])
+
+    def test_strongly_connected_components_of_size_3(self):
+        g = create_graph(7, [(0, 1), (1, 2), (2, 0), (3, 4), (4, 3), (3, 5), (5, 6), (6, 3)])
+        self.assertEqual(strongly_connected_components(g), [[2, 1, 0], [6, 5, 4, 3]])
+
+
+if __name__ == '__main__':
+    unittest.main()
