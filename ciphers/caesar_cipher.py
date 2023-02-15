be8f2629eb57bf7e0e5f361dd036794ae312ca6d
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-15 16:37:33 +0700

    feat(algos/ciphers): implement basic cipher algorithms: - Caesar cipher - Vigenere cipher - Enigma engine cipher - Todo: add more cipher algorithms, add docs

---
+++ ciphers/caesar_cipher.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def caesar_cipher(text: str, key: int, encrypt: bool = True) -> str:
+    """Encrypt or decrypt text using the Caesar cipher."""
+    result = ""
+    for char in text:
+        # Convert each character to its ASCII code
+        code = ord(char)
+        # Shift the code by the key (positive for encryption, negative for decryption)
+        shift = key if encrypt else -key
+        new_code = (code - 65 + shift) % 26 + 65 if char.isupper() \
+            else (code - 97 + shift) % 26 + 97 if char.islower() else code
+        # Convert the new code back to a character and add it to the result
+        result += chr(new_code)
+    return result
+
+
+class TestCaesarCipher(unittest.TestCase):
+
+    def test_encrypt(self):
+        self.assertEqual(caesar_cipher("HELLO WORLD", 3), "KHOOR ZRUOG")
+        self.assertEqual(caesar_cipher("PYTHON IS GREAT", 7), "WFAOVU PZ NYLHA")
+        self.assertEqual(caesar_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 25), "ZABCDEFGHIJKLMNOPQRSTUVWXY")
+        self.assertEqual(caesar_cipher("hello world", 10), "rovvy gybvn")
+        self.assertEqual(caesar_cipher("1234!@#$", 5), "1234!@#$")
+
+    def test_decrypt(self):
+        self.assertEqual(caesar_cipher("KHOOR ZRUOG", 3, encrypt=False), "HELLO WORLD")
+        self.assertEqual(caesar_cipher("WFAOVU PZ NYLHA", 7, encrypt=False), "PYTHON IS GREAT")
+        self.assertEqual(caesar_cipher("ZABCDEFGHIJKLMNOPQRSTUVWXY", 25, encrypt=False), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.assertEqual(caesar_cipher("rovvy gybvn", 10, encrypt=False), "hello world")
+        self.assertEqual(caesar_cipher("1234!@#$", 5, encrypt=False), "1234!@#$")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ciphers/vigenere_cipher.py
@@ -0,0 +1,74 @@
+import unittest
+
+
+def vigenere_cipher(plaintext: str, keyword: str, encrypt: bool = True) -> str:
+    """
+    Encrypts or decrypts a plaintext message using the Vigenère cipher with a given keyword.
+
+    Args:
+        plaintext: The plaintext message to encrypt or decrypt.
+        keyword: The keyword to use for the Vigenère cipher.
+        encrypt: If True, encrypt the plaintext message (default). If False, decrypt the ciphertext message.
+
+    Returns:
+        The encrypted or decrypted message as a string.
+
+    """
+    key = ""
+    for i in range(len(plaintext)):
+        key += keyword[i % len(keyword)]
+    # generate the key string by repeating the keyword over the length of the plaintext
+
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            shift = ord(key[i].upper()) - 65
+            # convert the current key letter to a shift value (A=0, B=1, etc.)
+            if not encrypt:
+                shift = 26 - shift
+                # if decrypting, invert the shift value
+            char = chr((ord(plaintext[i].upper()) - 65 + shift) % 26 + 65)
+            # encrypt or decrypt the current plaintext letter using a Caesar cipher with the current shift value
+            ciphertext += char
+        else:
+            ciphertext += plaintext[i]
+            # non-alphabetic characters are copied directly to the ciphertext
+
+    return ciphertext
+
+
+class TestVigenereCipher(unittest.TestCase):
+
+    def test_encrypt(self):
+        plaintext = "HELLO WORLD"
+        keyword = "SECRET"
+        ciphertext = vigenere_cipher(plaintext, keyword)
+        self.assertEqual(ciphertext, "ZINCS OSTCH")
+
+    def test_decrypt(self):
+        ciphertext = "ZINCS OSTCH"
+        keyword = "SECRET"
+        plaintext = vigenere_cipher(ciphertext, keyword, encrypt=False)
+        self.assertEqual(plaintext, "HELLO WORLD")
+
+    def test_uppercase(self):
+        plaintext = "HELLO WORLD"
+        keyword = "SECRET"
+        ciphertext = vigenere_cipher(plaintext, keyword)
+        self.assertEqual(ciphertext, ciphertext.upper())
+
+    def test_non_alpha(self):
+        plaintext = "HELLO WORLD!"
+        keyword = "SECRET"
+        ciphertext = vigenere_cipher(plaintext, keyword)
+        self.assertEqual(ciphertext, "ZINCS OSTCH!")
+
+    def test_long_keyword(self):
+        plaintext = "HELLO WORLD"
+        keyword = "VERYLONGKEYWORD"
+        ciphertext = vigenere_cipher(plaintext, keyword)
+        self.assertEqual(ciphertext, "CICJZ JUBPB")
+
+
+if __name__ == '__main__':
+    unittest.main()
