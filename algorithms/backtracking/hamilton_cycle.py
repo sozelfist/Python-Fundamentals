f5536b0827f6c17d5678f5614514e95fb7de55a9
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-24 15:32:48 +0700

    feat(algos/backtrack)[graph]: implement hamilton sycle

---
+++ algorithms/backtracking/hamilton_cycle.py
@@ -0,0 +1,61 @@
+from typing import List
+import unittest
+
+
+def hamilton_cycle(graph: List[List[int]], start: int) -> List[int] | None:
+    def backtrack(curr_node: int, visited: List[bool], path: List[int]) -> List[int] | None:
+        # If all vertices have been visited and the last vertex is adjacent to the start vertex,
+        # return the path with the start vertex appended to form a cycle
+        if len(path) == len(graph) and graph[curr_node][start] == 1:
+            return path + [start]
+
+        for next_node in range(len(graph)):
+            if graph[curr_node][next_node] == 1 and not visited[next_node]:
+                visited[next_node] = True
+                path.append(next_node)
+                res = backtrack(next_node, visited, path)
+                if res is not None:
+                    return res
+                visited[next_node] = False
+                path.pop()
+
+        return None
+
+    visited = [False] * len(graph)
+    visited[start] = True
+    path = [start]
+    return backtrack(start, visited, path)
+
+
+class TestHamiltonCycle(unittest.TestCase):
+    def test_cycle_exists(self):
+        graph = [[0, 1, 1, 0, 0],
+                 [1, 0, 1, 1, 0],
+                 [1, 1, 0, 1, 1],
+                 [0, 1, 1, 0, 1],
+                 [0, 0, 1, 1, 0]]
+        cycle = hamilton_cycle(graph, 0)
+        self.assertEqual(cycle, [0, 1, 3, 4, 2, 0])
+
+    def test_cycle_does_not_exist(self):
+        graph = [[0, 1, 1, 0, 0],
+                 [1, 0, 1, 1, 0],
+                 [1, 1, 0, 1, 1],
+                 [0, 1, 1, 0, 0],
+                 [0, 0, 1, 0, 0]]
+        cycle = hamilton_cycle(graph, 0)
+        self.assertIsNone(cycle)
+
+    def test_single_vertex(self):
+        graph = [[1]]
+        cycle = hamilton_cycle(graph, 0)
+        self.assertEqual(cycle, [0, 0])
+
+    def test_two_vertices_cycle(self):
+        graph = [[0, 1], [1, 0]]
+        cycle = hamilton_cycle(graph, 1)
+        self.assertEqual(cycle, [1, 0, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
