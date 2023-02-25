76ce0e9fa23e8514d2583c223424b1ac91abef04
sozelsgd <nhantruongnguyen123@gmail.com>
2023-02-25 08:57:45 +0700

    feat(caching): implement Least Recently Used (LRU) cache

---
+++ caching/LRU_cache.py
@@ -0,0 +1,80 @@
+from typing import Dict, List, Tuple, Optional
+import unittest
+
+
+class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.cache: Dict[int, Tuple[int, int]] = {}
+        self.queue: List[int] = []
+
+    def get(self, key: int) -> Optional[int]:
+        if key not in self.cache:
+            return None
+
+        # Move key to front of queue
+        self.queue.remove(key)
+        self.queue.insert(0, key)
+
+        # Return value associated with key
+        return self.cache[key][1]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update value and move key to front of queue
+            self.cache[key] = (self.cache[key][0], value)
+            self.queue.remove(key)
+            self.queue.insert(0, key)
+        else:
+            # Add new key-value pair to cache and queue
+            self.cache[key] = (len(self.queue), value)
+            self.queue.insert(0, key)
+
+            # Evict least recently used item if cache is full
+            if len(self.queue) > self.capacity:
+                lru_key = self.queue.pop()
+                del self.cache[lru_key]
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_capacity_zero(self):
+        cache = LRUCache(0)
+        self.assertIsNone(cache.get(1))
+        cache.put(1, 1)
+        self.assertIsNone(cache.get(1))
+
+    def test_capacity_one(self):
+        cache = LRUCache(1)
+        cache.put(1, 1)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(2, 2)
+        self.assertIsNone(cache.get(1))
+        self.assertEqual(cache.get(2), 2)
+
+    def test_capacity_two(self):
+        cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(3, 3)
+        self.assertIsNone(cache.get(2))
+        self.assertEqual(cache.get(1), 1)
+        cache.put(4, 4)
+        self.assertIsNone(cache.get(3))
+        self.assertEqual(cache.get(1), 1)
+        self.assertEqual(cache.get(4), 4)
+
+    def test_cache_update(self):
+        cache = LRUCache(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(2, 3)
+        self.assertEqual(cache.get(2), 3)
+        cache.put(1, 4)
+        self.assertEqual(cache.get(2), 3)
+        self.assertEqual(cache.get(1), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
