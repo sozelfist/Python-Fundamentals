dc0d98afc916aea4825e0cf649a918c3f08d84a2
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2023-11-09 08:22:01 +0700

    ref: add docstring to `Vector` class and unit test cases

--- advanced/oop/basic_vector.py
+++ advanced/oop/basic_vector.py
@@ -1,25 +1,83 @@
+import unittest
+
+
 class Vector:
+    """
+    A simple 2D vector class with basic operations.
+    """
+
     def __init__(self, x: float, y: float):
+        """
+        Initializes a 2D vector with the provided x and y coordinates.
+
+        Args:
+            x (float): The x-coordinate of the vector.
+            y (float): The y-coordinate of the vector.
+        """
         self.x = x
         self.y = y
 
     def __str__(self):
+        """
+        Returns the string representation of the vector.
+        """
         return f"({self.x}, {self.y})"
 
     def __add__(self, other):
+        """
+        Defines the addition operation between two vectors.
+
+        Args:
+            other (Vector): The other vector to be added.
+
+        Returns:
+            Vector: The resulting vector after addition.
+        """
         return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
+        """
+        Defines the subtraction operation between two vectors.
+
+        Args:
+            other (Vector): The other vector to be subtracted.
+
+        Returns:
+            Vector: The resulting vector after subtraction.
+        """
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, scalar: float):
+        """
+        Defines the scalar multiplication operation for a vector.
+
+        Args:
+            scalar (float): The scalar value to multiply the vector.
+
+        Returns:
+            Vector: The resulting vector after scalar multiplication.
+        """
         return Vector(self.x * scalar, self.y * scalar)
 
 
-if __name__ == '__main__':
+class TestVector(unittest.TestCase):
+    def test_addition(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(3, 4)
+        result = v1 + v2
+        self.assertEqual(str(result), "(4, 6)")
 
-    v1 = Vector(1, 2)
-    v2 = Vector(3, 4)
-    print(v1 + v2)  # (4, 6)
-    print(v1 - v2)  # (-2, -2)
-    print(v1 * 2)  # (2, 4)
+    def test_subtraction(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(3, 4)
+        result = v1 - v2
+        self.assertEqual(str(result), "(-2, -2)")
+
+    def test_scalar_multiplication(self):
+        v1 = Vector(1, 2)
+        result = v1 * 2
+        self.assertEqual(str(result), "(2, 4)")
+
+
+if __name__ == "__main__":
+    unittest.main()
